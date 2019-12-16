f4fd49dfa815b919ed79cbf15a766234d6ecfe9d
Asad <asadullahkust@gmail.com>
2019-12-16 22:16:58 +0500

    added splash screen and modified the ios and android splash screen

--- ios/mobile/AppDelegate.m
+++ ios/mobile/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"  // here
 
 @implementation AppDelegate
 
@@ -27,6 +28,9 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];  // here
+  // or
+  //[RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];
   return YES;
 }
 
